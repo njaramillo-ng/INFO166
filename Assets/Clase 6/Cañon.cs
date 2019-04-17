f34acc1390e9afde8672c943d65ba303820f3996
njaramillo-ng <njaramillo@nemorisgames.com>
2019-04-17 18:42:47 -0400

    clase 6: cañon completo

---
+++ Assets/Clase 6/Cañon.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cañon : MonoBehaviour {
+	public float velocidadRotacion = 50;
+	public float fuerzaDisparo = 20;
+	public GameObject bala;
+	public Transform bocaCañon;
+	public Transform ejeCañon;
+	
+	void Update () {
+		//Rotar horizontal (torreta)
+		transform.Rotate(0,Input.GetAxis("Horizontal") * Time.deltaTime * velocidadRotacion,0);
+
+		//Rotar vertical (eje cañon)
+		//se guarda la rotacion actual del eje en grados (eulerAngles)
+		Vector3 rot = ejeCañon.eulerAngles;
+		//se modifica la rotacion del eje actual en grados en el eje x aplicando la nueva posicion esperada, pero utilizando Clamp para mantenerla entre 0 y 50
+		//los valores de los ejes y & z se mantienen iguales
+		ejeCañon.eulerAngles = new Vector3(Mathf.Clamp(rot.x + Input.GetAxis("Vertical") * Time.deltaTime * velocidadRotacion/2, 0, 50),rot.y,rot.z);
+
+		//disparar
+		if(Input.GetKeyDown(KeyCode.Space)){
+			//instancia una copia del prefab bala y lo guarda en un gameobject nuevo
+			GameObject nuevaBala = (GameObject)Instantiate(bala,bocaCañon.position,bocaCañon.rotation);
+			//obtiene el rigidbody de la bala nueva
+			Rigidbody rbBala = nuevaBala.GetComponent<Rigidbody>();
+			//le aplica una fuerza al rigidbody de la bala
+			rbBala.AddForce(bocaCañon.forward * fuerzaDisparo,ForceMode.Impulse);
+		}	
+	}
+}
---
+++ Assets/Clase 6/UFO.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UFO : MonoBehaviour {
+	public int vida = 3;
+	public float velocidad;
+	public int direccion = 1;
+	
+	void Update () {
+		//movimiento considerando direccion
+		transform.Translate(velocidad * Time.deltaTime * direccion,0,0);
+		//invertir direccion al llegar al limite (8 y -8)
+		if(transform.position.x >= 8)
+			direccion = -1;
+		else if(transform.position.x <= -8)
+			direccion = 1;
+		
+		//revisar si la vida llega a 0 para destruirse
+		if(vida <= 0)
+			Destroy(gameObject);
+	}
+
+	void OnCollisionEnter(Collision c){
+		//si es golpeado por una bala, reduce su vida en 1 y destruye la bala
+		if(c.collider.tag == "Bala"){
+			vida--;
+			Destroy(c.collider.gameObject);
+		}
+	}
+}
